61bc2dc5142132a6aeb0046679bfd98af726c6e7
Walid Omran <71230057+WalidOmran@users.noreply.github.com>
2026-01-08 14:31:48 +0200

    Fix api/index.js
    Added body parser and rewriter middleware to the server.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-server.use(middlewares)
-server.use(router)
-module.exports = server
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
+server.use(middlewares);
+
+module.exports = server;
