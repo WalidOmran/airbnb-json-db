503c338908ce444bc98b5542d05abf6097719e30
Walid Omran <71230057+WalidOmran@users.noreply.github.com>
2026-01-15 14:23:16 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const path = require('path');
-// استخدام path.resolve بيضمن إن Vercel يوصل للملف في الـ Build
+
 const router = jsonServer.router(path.resolve(__dirname, '../db.json'));
 const middlewares = jsonServer.defaults();
 
@@ -11,5 +11,5 @@ server.use(jsonServer.rewriter({
 }));
 server.use(router);
 
-// مهم: في Vercel بنعمل export للسيرفر من غير ما ننادي listen يدوياً
 module.exports = server;
+
